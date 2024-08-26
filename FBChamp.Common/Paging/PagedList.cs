edc34393e7ed545c3c5c5652a7669fb6999a2bff
Georgi <georgi.ginduzov17d@gmail.com>
2024-08-26 08:57:08 +0300

    Refactor "Common" library

--- FBChamp.Common/Paging/PagedList.cs
+++ FBChamp.Common/Paging/PagedList.cs
@@ -7,11 +7,13 @@ public class PagedList<T> : PagedListBase
     public IReadOnlyList<T> Items { get; }
 
     public int From => (Page - 1) * PerPage + 1;
+
     public int To => From + Items.Count - 1;
 
     public PagedList(IList<T> items, int total, int page = 1, int perPage = int.MaxValue) : this(items, total,
         new PageInfo(page, perPage))
     {
+
     }
 
     public PagedList(IList<T> items, int total, PageInfo pageInfo) : base(total, pageInfo)
