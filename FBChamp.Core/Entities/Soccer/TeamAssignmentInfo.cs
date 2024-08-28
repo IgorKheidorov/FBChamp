2934b34d37f970058e0163ead53755c9f824fe0f
VaNkOSl <ivan_petrov0341@abv.bg>
2024-08-28 10:30:52 +0300

    Create class and repository

---
+++ FBChamp.Core/Entities/Soccer/TeamAssignmentInfo.cs
@@ -0,0 +1,16 @@
+﻿namespace FBChamp.Core.Entities.Soccer;
+
+public class TeamAssignmentInfo : Entity<Guid>
+{
+    public Guid LeagueId { get; set; }
+
+    public TeamAssignmentInfo()
+    {
+    }
+
+    public TeamAssignmentInfo(Guid teamId, Guid leagueId)
+    {
+        Id = teamId;
+        LeagueId = leagueId;
+    }
+}
