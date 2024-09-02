9a776dd12f4495571f167c0ee1a8b9608ef45169
vad3n <veselin.ym86@gmail.com>
2024-09-02 18:15:30 +0300

    Add Team integration tests

---
+++ IntegrationTests/TeamTests/TeamRepositoryTests.cs
@@ -0,0 +1,101 @@
+﻿using FBChamp.Core.Entities.Soccer;
+using FBChamp.Infrastructure;
+using IntegrationTests.Helpers;
+
+namespace IntegrationTests.TeamTests;
+
+[TestClass]
+public class TeamRepositoryTests
+{
+    [TestMethod]
+    public void AddTeams()
+    {
+        UnitOfWork unitOfWork = new UnitOfWork();
+        var initialCount = unitOfWork.GetAllTeamModels().Count;
+
+        Guid teamId = Guid.NewGuid();
+        Guid locationId = Guid.NewGuid();
+        Guid stadiumId = Guid.NewGuid();
+        var photoGenerator = new PhotoGenerator();
+        var photo = photoGenerator.Generate(300, 500);
+
+        List<Task> tasks = Enumerable.Range(0, 100)
+            .Select(x => new Team(teamId, "Name" + x, locationId, photo, stadiumId))
+            .Select(x => new Task(() =>
+            {
+                unitOfWork.Commit(x);
+            })).ToList();
+
+        tasks.ForEach(x => x.Start());
+        Task.WaitAll(tasks.ToArray());
+
+        var realCount = unitOfWork.GetAllTeamModels().Count;
+
+        Assert.AreEqual(initialCount + 1, realCount);
+    }
+
+    [TestMethod]
+    public void GetTeamById()
+    {
+        UnitOfWork unitOfWork = new UnitOfWork();
+        var teamId = Guid.NewGuid();
+        Guid locationId = Guid.NewGuid();
+        Guid stadiumId = Guid.NewGuid();
+        var photoGenerator = new PhotoGenerator();
+        var photo = photoGenerator.Generate(300, 500);
+
+        var team = new Team(teamId, "Name", locationId, photo, stadiumId);
+        unitOfWork.Commit(team);
+        var retrievedTeam = unitOfWork.GetTeamModel(teamId);
+
+        Assert.IsNotNull(retrievedTeam);
+        Assert.AreEqual(teamId, retrievedTeam.Team.Id);
+        Assert.AreEqual("Name", retrievedTeam.Team.Name);
+        Assert.AreEqual(photo, retrievedTeam.Team.Photo);
+    }
+
+    [TestMethod]
+    public void UpdateTeam()
+    {
+        UnitOfWork unitOfWork = new UnitOfWork();
+        var teamId = Guid.NewGuid();
+        Guid locationId = Guid.NewGuid();
+        Guid stadiumId = Guid.NewGuid();
+        var photoGenerator = new PhotoGenerator();
+        var photo = photoGenerator.Generate(300, 500);
+
+        var team = new Team(teamId, "Name", locationId, photo, stadiumId);
+        unitOfWork.Commit(team);
+        var retrievedTeam = unitOfWork.GetTeamModel(teamId);
+
+        Assert.AreEqual("Name", retrievedTeam.Team.Name);
+
+        retrievedTeam.Team.Name = "NewName";
+
+        unitOfWork.Commit(retrievedTeam.Team);
+        var updatedTeam = unitOfWork.GetTeamModel(teamId);
+
+        Assert.IsNotNull(updatedTeam);
+        Assert.AreEqual("NewName", updatedTeam.Team.Name);
+    }
+
+    [TestMethod]
+    public void DeleteTeam()
+    {
+        UnitOfWork unitOfWork = new UnitOfWork();
+        var teamId = Guid.NewGuid();
+        Guid locationId = Guid.NewGuid();
+        Guid stadiumId = Guid.NewGuid();
+        var photoGenerator = new PhotoGenerator();
+        var photo = photoGenerator.Generate(300, 500);
+
+        var team = new Team(teamId, "Name", locationId, photo, stadiumId);
+        unitOfWork.Commit(team);
+        var initialCount = unitOfWork.GetAllTeamModels().Count;
+
+        unitOfWork.Remove(teamId, typeof(Team));
+        var realCount = unitOfWork.GetAllTeamModels().Count;
+
+        Assert.AreEqual(initialCount - 1, realCount);
+    }
+}
---
+++ IntegrationTests/TeamTests/TeamValidatorTests.cs
@@ -0,0 +1,106 @@
+﻿using FBChamp.Core.Entities.Soccer;
+using FBChamp.Core.UnitOfWork;
+using FBChamp.Infrastructure;
+using IntegrationTests.Helpers;
+using System.Globalization;
+
+namespace IntegrationTests.TeamTests;
+
+[TestClass]
+public class TeamValidatorTests
+{
+    [TestInitialize]
+    public void Initialize()
+    {
+        Infrastructure.CleanUp();
+    }
+
+    [TestMethod]
+    [DataRow(257)]
+    [DataRow(300)]
+    public void TeamInvalidNameLengthTest(int nameLength)
+    {
+        UnitOfWork unitOfWork = new UnitOfWork();
+        var photoGenerator = new PhotoGenerator();
+        var photo = photoGenerator.Generate(300, 500);
+        Team team = new Team(Guid.NewGuid(), new string('n', nameLength), Guid.NewGuid(), photo, Guid.NewGuid());
+
+        var actualResult = unitOfWork.Commit(team);
+        Assert.AreEqual(CRUDResult.EntityValidationFailed, actualResult);
+    }
+
+    [TestMethod]
+    [DataRow(1)]
+    [DataRow(100)]
+    [DataRow(255)]
+    public void TeamValidNameLengthTest(int nameLength)
+    {
+        UnitOfWork unitOfWork = new UnitOfWork();
+        var photoGenerator = new PhotoGenerator();
+        var photo = photoGenerator.Generate(300, 500);
+        Team team = new Team(Guid.NewGuid(), new string('n', nameLength), Guid.NewGuid(), photo, Guid.NewGuid());
+
+        var actualResult = unitOfWork.Commit(team);
+        Assert.AreEqual(CRUDResult.Success, actualResult);
+    }
+
+    [TestMethod]
+    [DataRow(257)]
+    [DataRow(300)]
+    public void TeamInvalidDescriptionLengthTest(int descriptionLength)
+    {
+        UnitOfWork unitOfWork = new UnitOfWork();
+        var photoGenerator = new PhotoGenerator();
+        var photo = photoGenerator.Generate(300, 500);
+        Team team = new Team(Guid.NewGuid(), new string('n', 100), Guid.NewGuid(), photo, Guid.NewGuid(), new string('d', descriptionLength));
+
+        var actualResult = unitOfWork.Commit(team);
+        Assert.AreEqual(CRUDResult.EntityValidationFailed, actualResult);
+    }
+
+    [TestMethod]
+    [DataRow(1)]
+    [DataRow(100)]
+    [DataRow(255)]
+    public void TeamValidDescriptionLengthTest(int descriptionLength)
+    {
+        UnitOfWork unitOfWork = new UnitOfWork();
+        var photoGenerator = new PhotoGenerator();
+        var photo = photoGenerator.Generate(300, 500);
+        Team team = new Team(Guid.NewGuid(), new string('n', 100), Guid.NewGuid(), photo, Guid.NewGuid(), new string('d', descriptionLength));
+
+        var actualResult = unitOfWork.Commit(team);
+        Assert.AreEqual(CRUDResult.Success, actualResult);
+    }
+
+    [TestMethod]
+    [DataRow(100, 100)]
+    [DataRow(100, 500)]
+    [DataRow(300, 100)]
+    public void TeamInvalidPhotoSizeTest(int width, int height)
+    {
+        var unitOfWork = new UnitOfWork();
+        var photoGenerator = new PhotoGenerator();
+        var photo = photoGenerator.Generate(width, height);
+
+        var team = new Team(Guid.NewGuid(), new string('n', 100), Guid.NewGuid(), photo, Guid.NewGuid());
+
+        var actualResult = unitOfWork.Commit(team);
+
+        Assert.AreEqual(CRUDResult.EntityValidationFailed, actualResult);
+    }
+
+    [TestMethod]
+    public void TeamValidPhotoSizeTest()
+    {
+        var unitOfWork = new UnitOfWork();
+        var photoGenerator = new PhotoGenerator();
+        var photo = photoGenerator.Generate(300, 500);
+
+        var team = new Team(Guid.NewGuid(), new string('n', 100), Guid.NewGuid(), photo, Guid.NewGuid());
+
+        var actualResult = unitOfWork.Commit(team);
+
+        Assert.AreEqual(CRUDResult.Success, actualResult);
+    }
+}
