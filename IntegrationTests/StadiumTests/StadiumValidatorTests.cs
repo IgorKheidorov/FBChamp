d6f092a1699e2cc4b7c93143836475c11ff3d677
vad3n <veselin.ym86@gmail.com>
2024-09-02 16:00:42 +0300

    Add Stadium integration tests

---
+++ IntegrationTests/StadiumTests/StadiumValidatorTests.cs
@@ -0,0 +1,25 @@
+﻿using FBChamp.Core.Entities.Soccer;
+using FBChamp.Core.UnitOfWork;
+using FBChamp.Infrastructure;
+
+namespace IntegrationTests.StadiumTests;
+
+[TestClass]
+public class StadiumValidatorTests
+{
+    [TestInitialize]
+    public void Initialize()
+    {
+        Infrastructure.CleanUp();
+    }
+
+    [TestMethod]
+    public void StadiumInvalidNameNullTest()
+    {
+        UnitOfWork unitOfWork = new UnitOfWork();
+        Stadium stadium = new Stadium(new Guid(), null, new Guid());
+
+        var actualResult = unitOfWork.Commit(stadium);
+        Assert.AreEqual(CRUDResult.EntityValidationFailed, actualResult);
+    }
+}
