de8529a68058ce3d0d22a90a49d1fbf3efbc365c
Georgi <georgi.ginduzov17d@gmail.com>
2024-09-03 09:50:37 +0300

    Add locations validators

---
+++ FBChamp.Infrastructure/Validators/CountryValidator.cs
@@ -0,0 +1,15 @@
+﻿using FBChamp.Core.DataValidator;
+using FBChamp.Core.Entities;
+using FBChamp.Core.Entities.Soccer;
+using FBChamp.Core.UnitOfWork;
+
+namespace FBChamp.Infrastructure.Validators
+{
+    public class CountryValidator : IValidateEntity
+    {
+        public Type GetValidatedType() => typeof(Country);
+
+        public CRUDResult Validate(Entity entity) =>
+        entity is Country country ? country.Name is not null ? CRUDResult.Success : CRUDResult.EntityValidationFailed : CRUDResult.InvalidOperation;
+    }
+}
