3846f2e40ca4c490cb172e5f821324db4553d192
Georgi <georgi.ginduzov17d@gmail.com>
2024-09-03 09:50:23 +0300

    Add Repositories

---
+++ FBChamp.Infrastructure/Repositories/Membership/LocationAssignmentInfoRepository.cs
@@ -0,0 +1,12 @@
+﻿using System.Text.Json;
+using System.Linq.Expressions;
+using FBChamp.Core.Entities.Soccer;
+using FBChamp.Core.Repositories.Membership;
+
+namespace FBChamp.Infrastructure.Repositories.Membership
+{
+    internal class LocationAssignmentInfoRepository : JSONRepository<LocationAssignmentInfo, Guid>, ILocationAssignmentInfoRepository
+    {
+        protected override string JSONFileName => "Countries.json";        
+    }
+}
---
+++ FBChamp.Infrastructure/Repositories/Membership/LocationRepository.cs
@@ -0,0 +1,10 @@
+﻿using FBChamp.Core.Entities.Soccer;
+using FBChamp.Core.Repositories.Membership;
+
+namespace FBChamp.Infrastructure.Repositories.Membership
+{
+    internal class LocationRepository : JSONRepository<Location, Guid>, ILocationRepository
+    {
+        protected override string JSONFileName => "Countries.json";
+    }
+}
