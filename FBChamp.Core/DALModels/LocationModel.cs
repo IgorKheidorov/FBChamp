1a8495832222931938a229298070d9782e2de3c0
Georgi <georgi.ginduzov17d@gmail.com>
2024-09-03 09:48:59 +0300

    Add DAL Models

---
+++ FBChamp.Core/DALModels/LocationModel.cs
@@ -0,0 +1,19 @@
+﻿using FBChamp.Core.Entities.Soccer;
+
+namespace FBChamp.Core.DALModels
+{
+    public class LocationModel : EntityModel
+    {
+        public Location Location { get; init; }
+
+        public LocationModel(Guid id)
+        {
+            Location = new Location { Id = id };
+        }
+        
+        public LocationModel(Guid id, string city, Guid countryId, Country country)
+        {
+            Location = new Location(id, city, countryId, country);
+        }
+    }
+}
