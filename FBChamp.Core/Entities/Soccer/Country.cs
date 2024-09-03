51536a2979b7678e4027bbf378dfc96dcd571659
Georgi <georgi.ginduzov17d@gmail.com>
2024-09-03 09:48:49 +0300

    Add entities

---
+++ FBChamp.Core/Entities/Soccer/Country.cs
@@ -0,0 +1,7 @@
+﻿namespace FBChamp.Core.Entities.Soccer
+{
+    public class Country : Entity<Guid>
+    {
+        public string Name { get; set; }
+    }
+}
---
+++ FBChamp.Core/Entities/Soccer/Location.cs
@@ -0,0 +1,24 @@
+﻿namespace FBChamp.Core.Entities.Soccer
+{
+    public class Location : Entity<Guid>
+    {
+        public Guid Id { get; set; }
+        public string City { get; set; }
+        public Guid CountryId { get; set; }
+        public Country Country { get; set; }
+
+        public Location()
+        {
+            
+        }
+
+        public Location(Guid id, string city, Guid countryId, Country country)
+        {
+            Id = id;
+            City = city is not null ? city : "Not assigned value";
+            CountryId = countryId;
+            Country = country is not null ? country : new Country();
+        }
+
+    }
+}
