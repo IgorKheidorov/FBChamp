acf7db09a80812dedf3a90e6a027705361d77818
Georgi <georgi.ginduzov17d@gmail.com>
2024-09-03 09:50:37 +0300

    Add locations validators

---
+++ FBChamp.Infrastructure/Validators/LocationValidator.cs
@@ -0,0 +1,15 @@
+﻿using FBChamp.Core.DataValidator;
+using FBChamp.Core.Entities;
+using FBChamp.Core.Entities.Soccer;
+using FBChamp.Core.UnitOfWork;
+
+namespace FBChamp.Infrastructure.Validators
+{
+    public class LocationValidator : IValidateEntity
+    {
+        public Type GetValidatedType() => typeof(LocationValidator);
+
+        public CRUDResult Validate(Entity entity) =>
+        entity is Location location ? location.City is not null ? CRUDResult.Success : CRUDResult.EntityValidationFailed : CRUDResult.InvalidOperation;
+    }
+}
