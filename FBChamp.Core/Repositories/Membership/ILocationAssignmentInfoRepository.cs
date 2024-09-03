eb6e0c7ff8e1f08e49c5d17b153974e4f46cb3ee
Georgi <georgi.ginduzov17d@gmail.com>
2024-09-03 09:49:17 +0300

    Refactor repositories

---
+++ FBChamp.Core/Repositories/Membership/ILocationAssignmentInfoRepository.cs
@@ -0,0 +1,8 @@
+﻿using FBChamp.Core.Entities.Soccer;
+
+namespace FBChamp.Core.Repositories.Membership
+{
+    public interface ILocationAssignmentInfoRepository : IRepository<LocationAssignmentInfo, Guid>
+    {
+    }
+}
