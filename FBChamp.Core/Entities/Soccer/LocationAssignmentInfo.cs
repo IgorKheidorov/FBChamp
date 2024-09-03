f63397c59dd2ee3a455c07aa68602982ec4016b9
Georgi <georgi.ginduzov17d@gmail.com>
2024-09-03 09:48:49 +0300

    Add entities

---
+++ FBChamp.Core/Entities/Soccer/LocationAssignmentInfo.cs
@@ -0,0 +1,24 @@
+﻿using System.Security.Authentication;
+
+namespace FBChamp.Core.Entities.Soccer
+{
+    public class LocationAssignmentInfo : Entity<Guid>
+    {
+        public string City { get; set; }
+        public Guid CountryId { get; set; }
+        public Country Country { get; set; }
+
+        public LocationAssignmentInfo()
+        {
+            
+        }
+
+        public LocationAssignmentInfo(Guid cityId, string city, Guid countryId, Country country)
+        {
+            Id = cityId;
+            City = city;
+            CountryId = countryId;
+            Country = country;
+        }
+    }
+}
