c275c47bd314d6da0c52bdace10618ca84d9e521
Georgi <georgi.ginduzov17d@gmail.com>
2024-09-04 15:16:06 +0300

    Add location entity

---
+++ FBChamp.Core/Entities/Soccer/Location.cs
@@ -0,0 +1,11 @@
+﻿namespace FBChamp.Core.Entities.Soccer
+{
+    public class Location : Entity<Guid>
+    {
+        public string Country { get; set; }
+        public string City { get; set; }
+        public string Street { get; set; }
+    
+        public override string ToString() => $"{Country}, {City}, {Street}";
+    }
+}
