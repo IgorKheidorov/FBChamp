7e8dd12fd334001848909b3bfb37708795486022
VaNkOSl <ivan_petrov0341@abv.bg>
2024-09-05 16:31:48 +0300

    Create class , class model and repository

---
+++ FBChamp.Core/Entities/Soccer/Match.cs
@@ -0,0 +1,34 @@
+﻿using FBChamp.Core.Entities.Soccer.Enums;
+
+namespace FBChamp.Core.Entities.Soccer;
+
+public class Match : Entity<Guid>
+{
+    public Guid StadiumId { get; set; }
+
+    public Guid LeagueId { get; set; }
+
+    public MatchStatus Status { get; set; }
+
+    public DateTime StartTimeOfMatch { get; set; }
+
+    public Guid HostTeamId { get; set; }
+
+    public Guid GuestTeamId { get; set; }
+
+    public Match()
+    {
+    }
+
+    public Match(Guid matchId, Guid stadiumId, Guid leagueId, MatchStatus status,
+                DateTime startTimeOfMatch, Guid hostTeamId, Guid guestTeamId)
+    {
+        Id = matchId;
+        StadiumId = stadiumId;
+        LeagueId = leagueId;
+        Status = status;
+        StartTimeOfMatch = startTimeOfMatch;
+        HostTeamId = hostTeamId;
+        GuestTeamId = guestTeamId;
+    }
+}
---
+++ FBChamp.Infrastructure/Repositories/Membership/MatchRepository.cs
@@ -0,0 +1,9 @@
+﻿using FBChamp.Core.Entities.Soccer;
+using FBChamp.Core.Repositories.Membership;
+
+namespace FBChamp.Infrastructure.Repositories.Membership;
+
+internal class MatchRepository : JSONRepository<Match, Guid>, IMatchRepository
+{
+    protected override string JSONFileName => "Matches.json";
+}
