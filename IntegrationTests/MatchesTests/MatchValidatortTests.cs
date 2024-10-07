3b33c9e7cdd6d7840a34a87268a249c048ad999f
VaNkOSl <ivan_petrov0341@abv.bg>
2024-10-07 18:30:13 +0300

    Updated Match Integration Tests

--- IntegrationTests/MatchesTests/MatchValidatortTests.cs
+++ IntegrationTests/MatchesTests/MatchValidatortTests.cs
@@ -1,6 +1,7 @@
 ﻿using FBChamp.Core.Entities.Soccer;
 using FBChamp.Core.Entities.Soccer.Enums;
 using FBChamp.Core.UnitOfWork;
+using FBChamp.DataGenerators;
 using FBChamp.Infrastructure;
 using static IntegrationTests.TestDataFactory;
 
@@ -10,6 +11,7 @@ namespace IntegrationTests.MatchesTests;
 public class MatchValidatortTests
 {
     private IUnitOfWork? _unitOfWork;
+    private DataGenerator? _dataGenerator;
 
     [TestInitialize]
     public void Initialize()
@@ -17,38 +19,43 @@ public class MatchValidatortTests
         _unitOfWork = new UnitOfWork();
         Infrastructure.CleanUp();
 
-        SeedEntity(_unitOfWork);
+        _dataGenerator = new DataGenerator(_unitOfWork);
+
+        _dataGenerator.GenerateMatch(new Dictionary<string, string>
+        {
+            { "Count", "1" }
+        });
     }
 
     [TestMethod]
     public void AddMatch_ShouldAddMatchSuccessfully_WhenAllEntitiesAreValid()
     {
-        var match = Match1!;
-
         var allMatches = _unitOfWork!.GetAllMatchModels();
+        var match = allMatches.First();
 
         Assert.IsNotNull(allMatches, "The list of matches should be not null");
         Assert.AreEqual(allMatches.Count, 1, "There should be exactly one match");
 
-        var matchModel = _unitOfWork.GetMatchModel(match.Id);
+        var matchModel = _unitOfWork.GetMatchModel(match.Match.Id);
 
         Assert.IsNotNull(matchModel);
-        Assert.AreEqual(match.Status, MatchStatus.InProgress, "The match status should be InProgress");
-        Assert.AreEqual(match.GuestTeamId, matchModel.GuestTeam.Team.Id, "Guest team ID should match");
-        Assert.AreEqual(match.HostTeamId, matchModel.HostTeam.Team.Id, "Host team ID should match");
-        Assert.AreEqual(match.StadiumId, matchModel.Stadium.Stadium.Id, "Stadium ID should match");
-        Assert.AreEqual(match.LeagueId, matchModel.Match.LeagueId, "League ID should match");
+        Assert.AreEqual(match.Match.Status, MatchStatus.InProgress, "The match status should be InProgress");
+        Assert.AreEqual(match.Match.GuestTeamId, matchModel.GuestTeam.Team.Id, "Guest team ID should match");
+        Assert.AreEqual(match.Match.HostTeamId, matchModel.HostTeam.Team.Id, "Host team ID should match");
+        Assert.AreEqual(match.Match.StadiumId, matchModel.Stadium.Stadium.Id, "Stadium ID should match");
+        Assert.AreEqual(match.Match.LeagueId, matchModel.Match.LeagueId, "League ID should match");
     }
 
     [TestMethod]
     public void AddMatch_ShouldReturnEntityValidationFailed_WhenGuestTeamDoesNotExists()
     {
-        var match = Match1!;
+        var allMatches = _unitOfWork!.GetAllMatchModels();
+        var match = allMatches.First();
         var invalidGuestTeamId = Guid.NewGuid();
 
-        match.GuestTeamId = invalidGuestTeamId;
+        match.Match.GuestTeamId = invalidGuestTeamId;
 
-        var result = _unitOfWork!.Commit(match);
+        var result = _unitOfWork!.Commit(match.Match);
 
         Assert.AreEqual(CRUDResult.EntityValidationFailed, result, "Expected validation failure because guest team does not exists");
     }
@@ -56,12 +63,13 @@ public class MatchValidatortTests
     [TestMethod]
     public void AddMatch_ShouldReturnEntityValidationFailed_WhenHostTeamDoesNotExists()
     {
-        var match = Match1!;
+        var allMatches = _unitOfWork!.GetAllMatchModels();
+        var match = allMatches.First();
         var invalidHostTeamId = Guid.NewGuid();
 
-        match.HostTeamId = invalidHostTeamId;
+        match.Match.HostTeamId = invalidHostTeamId;
 
-        var result = _unitOfWork!.Commit(match);
+        var result = _unitOfWork!.Commit(match.Match);
 
         Assert.AreEqual(CRUDResult.EntityValidationFailed, result, "Expected validation failure because host team does not exists");
     }
@@ -69,12 +77,13 @@ public class MatchValidatortTests
     [TestMethod]
     public void AddMatch_ShouldReturnEntityValidationFailed_WhenLeagueDoesNotExists()
     {
-        var match = Match1!;
+        var allMatches = _unitOfWork!.GetAllMatchModels();
+        var match = allMatches.First();
         var invalidLeagueId = Guid.NewGuid();
 
-        match.LeagueId = invalidLeagueId;
+        match.Match.LeagueId = invalidLeagueId;
 
-        var result = _unitOfWork!.Commit(match);
+        var result = _unitOfWork!.Commit(match.Match);
 
         Assert.AreEqual(CRUDResult.EntityValidationFailed, result, "Expected validation failure because league does not exists");
     }
@@ -82,12 +91,13 @@ public class MatchValidatortTests
     [TestMethod]
     public void AddMatch_ShouldReturnEntityValidationFailed_WhenStadiumDoesNotExists()
     {
-        var match = Match1!;
+        var allMatches = _unitOfWork!.GetAllMatchModels();
+        var match = allMatches.First();
         var invalidStadiumId = Guid.NewGuid();
 
-        match.StadiumId= invalidStadiumId;
+        match.Match.StadiumId= invalidStadiumId;
 
-        var result = _unitOfWork!.Commit(match);
+        var result = _unitOfWork!.Commit(match.Match);
 
         Assert.AreEqual(CRUDResult.EntityValidationFailed, result, "Expected validation failure because stadium does not exists");
     }
@@ -95,32 +105,40 @@ public class MatchValidatortTests
     [TestMethod]
     public void UpdateMatch_ShouldUpdateMatchSuccessfully_WhenAllEntitiesAreValid()
     {
-        var match = Match1!;
+        var allMatches = _unitOfWork!.GetAllMatchModels();
+        var match = allMatches.First();
 
-        match.GuestTeamId = Team1!.Id;
-        match.HostTeamId = Team2!.Id;
-        match.LeagueId= League2!.Id;
-        match.Status = MatchStatus.Delayed;
+        var allTeamModels = _unitOfWork!.GetAllTeamModels();
+        var allLeagues = _unitOfWork.GetAllLeagueModels();
+        var hostTeam = allTeamModels.First();
+        var guestTeam = allTeamModels.Last();
+        var league = allLeagues.First();
 
-        var updateResult = _unitOfWork!.Commit(match);
-        var updatedMatch = _unitOfWork.GetMatchModel(match.Id);
+        match.Match.GuestTeamId = guestTeam.Team.Id;
+        match.Match.HostTeamId = guestTeam.Team.Id;
+        match.Match.LeagueId= league.League.Id;
+        match.Match.Status = MatchStatus.Delayed;
+
+        var updateResult = _unitOfWork!.Commit(match.Match);
+        var updatedMatch = _unitOfWork.GetMatchModel(match.Match.Id);
 
         Assert.IsNotNull(updateResult, "Update result should not be null");
-        Assert.AreEqual(match.LeagueId, updatedMatch.Match.LeagueId, "League ID should match");
-        Assert.AreEqual(match.GuestTeamId, updatedMatch.GuestTeam.Team.Id, "Guest Team ID should match");
-        Assert.AreEqual(match.HostTeamId, updatedMatch.HostTeam.Team.Id, "Host Team ID should match");
-        Assert.AreEqual(match.Status, updatedMatch.Match.Status, "Match status should match");
+        Assert.AreEqual(match.Match.LeagueId, updatedMatch.Match.LeagueId, "League ID should match");
+        Assert.AreEqual(match.Match.GuestTeamId, updatedMatch.GuestTeam.Team.Id, "Guest Team ID should match");
+        Assert.AreEqual(match.Match.HostTeamId, updatedMatch.HostTeam.Team.Id, "Host Team ID should match");
+        Assert.AreEqual(match.Match.Status, updatedMatch.Match.Status, "Match status should match");
     }
 
     [TestMethod]
     public void RemoveMatch_ShouldRemoveMatchSuccessfully_WhenMatchExists()
     {
-        var match = Match1!;
+        var allMatches = _unitOfWork!.GetAllMatchModels();
+        var match = allMatches.First();
 
-        var result = _unitOfWork!.Remove(match.Id, typeof(Match));
+        var result = _unitOfWork!.Remove(match.Match.Id, typeof(Match));
 
         Assert.AreEqual(result, CRUDResult.Success, "The match should be removed successfully.");
-        Assert.IsNull(_unitOfWork.GetMatchModel(match.Id), "The match should no longer exist in the repository.");
+        Assert.IsNull(_unitOfWork.GetMatchModel(match.Match.Id), "The match should no longer exist in the repository.");
     }
 
     [TestMethod]
